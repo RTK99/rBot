6d9e7b2eeec93069c8892d1cd723a65db6070340
RTK99 <richrancy@gmail.com>
2017-07-02 16:07:29 -0400

    added restart command

---
+++ commands/restart.js
@@ -0,0 +1,16 @@
+exports.run = (client, msg) => {
+    msg.edit('Restarting rBot... :thumbsup:').then(()=> {
+        process.exit(42);
+    });
+};
+
+exports.conf = {
+    enabled: true,
+    aliases: []
+};
+
+exports.help = {
+    name: 'restart',
+    description: 'Restarts the selfbot.',
+    usage: 'restart'
+};

900f7d847d82566affb205150655ad380dcc478c
rTexel <rramce3@outlook.com>
2017-09-15 20:08:54 -0400

    Added stats command

---
+++ v2/commands/core/stats.js
@@ -0,0 +1,21 @@
+const { Command } = require('ares.js');
+const { MessageEmbed } = require('discord.js');
+const { version: discordVersion } = require('discord.js');
+
+module.exports = class StatsCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'stats',
+            description: 'Shows some stats about rBot.',
+            aliases: ['info', 'uptime']
+        });
+    }
+
+    async run (message, args) {
+        let embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setAuthor('rBot Stats', this.client.user.displayAvatarURL())
+            .setDescription(`Memory Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\nUsers: ${this.client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\nChannels: ${this.client.channels.size.toLocaleString()}\nServers: ${this.client.guilds.size.toLocaleString()}\nDiscord.js version: v${discordVersion}\nNode.js version: ${process.version}`);
+        message.edit({embed});
+    }
+};
--- v2/run.js
+++ v2/run.js
@@ -3,7 +3,7 @@ const config = require('./config.json');
 const client = new Ares.Client({
     prefix: '>',
     selfbot: true,
-    disabledServers: ['349243932447604736']
+    disabledServers: ['349243932447604736', '200661830648070145', '96230004047740928']
 });
 
 client.registerCommands();

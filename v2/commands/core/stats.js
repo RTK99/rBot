dd21c9fd25a700509b5d585aa480f7df40e72f0e
rTexel <rramce3@outlook.com>
2017-09-15 20:15:37 -0400

    added bold

--- v2/commands/core/stats.js
+++ v2/commands/core/stats.js
@@ -17,7 +17,7 @@ module.exports = class StatsCommand extends Command {
             .setTimestamp()
             .setFooter('rBot')
             .setAuthor('rBot Stats', this.client.user.displayAvatarURL())
-            .setDescription(`**Memory Usage:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n**Users:** ${this.client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\n**Channels:** ${this.client.channels.size.toLocaleString()}\nServers: ${this.client.guilds.size.toLocaleString()}\n**Discord.js version:** v${discordVersion}\n**Node.js version:** ${process.version}`);
+            .setDescription(`**Memory Usage:** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\n**Users:** ${this.client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\n**Channels:** ${this.client.channels.size.toLocaleString()}\n**Servers:** ${this.client.guilds.size.toLocaleString()}\n**Discord.js version:** v${discordVersion}\n**Node.js version:** ${process.version}`);
         message.edit({embed});
     }
 };

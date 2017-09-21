3e8981e8a377f8de1df47b33f307cd5f870b1403
rTexel <rramce3@outlook.com>
2017-09-21 09:30:09 -0400

    let's try this

--- v2/run.js
+++ v2/run.js
@@ -13,16 +13,13 @@ client.login(config.token);
 
 client.on('message', message => {
     if (message.content.includes(message.client.user.username) || message.mentions.users.first() === message.client.user) {
-        message.client.emit('isMentioned', message);
+        const embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setAuthor(message.author.tag, message.author.displayAvatarURL())
+            .setDescription(message.content)
+            .setTimestamp()
+            .setFooter('Mention Tracker');
+        client.channels.get('360396387290185730').send({embed});
+      
     }
-});
-
-client.on('isMentioned', message => {
-    const embed = new MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor(message.author.tag, message.author.displayAvatarURL())
-        .setDescription(message.content)
-        .setTimestamp()
-        .setFooter('Mention Tracker');
-    client.channels.get('360396387290185730').send({embed});
 });

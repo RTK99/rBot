f2688a79a8406d62f659b2e63b6084b6a8b93fbd
rTexel <rramce3@outlook.com>
2017-09-21 14:13:41 +0000

    logging

--- v2/run.js
+++ v2/run.js
@@ -12,14 +12,14 @@ client.registerDefaultCommands();
 client.login(config.token);
 
 client.on('message', message => {
-    if (message.content.includes(message.client.user.username) || message.mentions.users.first() === message.client.user) {
+    if (!message.guild) return;
+    if (message.content.includes(message.client.user.username) || message.content.includes(message.guild.me.displayName) || message.mentions.users.first() && message.mentions.users.first().id === client.user.id) {
         const embed = new MessageEmbed()
             .setColor('RANDOM')
             .setAuthor(message.author.tag, message.author.displayAvatarURL())
             .setDescription(message.content)
             .setTimestamp()
             .setFooter('Mention Tracker');
-        client.channels.get('360396387290185730').send({embed});
-      
+        message.client.channels.get('360396387290185730').send('@everyone', {embed});
     }
 });

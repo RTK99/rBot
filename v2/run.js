8ee6d90458d1fe6a76beb8e69936ac81b0d5980c
rTexel <rramce3@outlook.com>
2017-09-21 10:17:26 -0400

    selfbot logs

--- v2/run.js
+++ v2/run.js
@@ -1,5 +1,6 @@
 const Ares = require('ares.js');
 const config = require('./config.json');
+const { WebhookClient } = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 const client = new Ares.Client({
     prefix: '>',
@@ -13,13 +14,14 @@ client.login(config.token);
 
 client.on('message', message => {
     if (!message.guild) return;
-    if (message.content.includes(message.client.user.username) || message.content.includes(message.guild.me.displayName) || message.mentions.users.first() && message.mentions.users.first().id === client.user.id) {
+    if (message.content.includes(message.client.user.username) || message.content.includes(message.guild.me.displayName)) {
+        let webhook = new WebhookClient(config.webhookID, config.webhookToken);
         const embed = new MessageEmbed()
             .setColor('RANDOM')
             .setAuthor(message.author.tag, message.author.displayAvatarURL())
             .setDescription(message.content)
             .setTimestamp()
             .setFooter('Mention Tracker');
-        message.client.channels.get('360396387290185730').send('@everyone', {embed});
+        webhook.send(client.user, {embed});
     }
 });
